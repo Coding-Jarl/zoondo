9675b6b0ac1527620b2a4009ea44ac672a6de5c4
Coding-Jarl <senvisage@gmail.com>
2023-10-09 21:45:27 +0200

    feat: my tokens can move according to their sample

--- src/components/Square.tsx
+++ src/components/Square.tsx
@@ -5,22 +5,41 @@ import Card from './Card'
 import styles from '@/styles/components/Square.module.scss'
 import { useDrop } from 'react-dnd'
 
+function canMove(
+  token: Game.CardWithPosition,
+  { x: toX, y: toY }: Position
+): boolean {
+  return token.moves
+    .flat()
+    .some(
+      ([itemX, itemY]) =>
+        itemX + token.x === toX &&
+        itemY + token.y === toY &&
+        (itemX !== 0 || itemY !== 0)
+    )
+}
+
 type Props = {
   card: Game.CardWithPosition | undefined
   x: number
   y: number
 }
-
 export default function Square({ card, x, y }: Props) {
   const cardFocus = useBoardStore((state) => state.cardFocus)
   const setCardFocus = useBoardStore((state) => state.setCardFocus)
   const moveCard = useBoardStore((state) => state.moveCard)
-  const [, drop] = useDrop(
+  const [{ isOver }, drop] = useDrop(
     () => ({
       accept: 'CLOBOULON',
       drop: (item: Game.CardWithPosition) => {
         if (!item) return
         moveCard(item, { x, y })
+      },
+      collect: (monitor) => ({
+        isOver: !!monitor.isOver(),
+      }),
+      canDrop: (item: Game.CardWithPosition) => {
+        return canMove(item, { x, y })
       },
     }),
     [card?.x, card?.y]
@@ -30,7 +49,11 @@ export default function Square({ card, x, y }: Props) {
     setCardFocus(card)
   }
 
-  const cssClasses = clsx(styles.wrapper, card ? styles.occupied : styles.empty)
+  const cssClasses = clsx(
+    styles.wrapper,
+    card ? styles.occupied : styles.empty,
+    isOver && styles.hovered
+  )
   return (
     <div ref={drop} className={cssClasses} onMouseEnter={hHover}>
       {card && <Card {...card} />}
